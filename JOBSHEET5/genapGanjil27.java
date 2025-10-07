83c73cdd7f9a3c04bc718c21518807349bbae9aa
salkeisha <keishasetiadi48@gmail.com>
2025-10-07 20:03:08 +0700

    Tugas 2

---
+++ JOBSHEET5/genapGanjil27.java
@@ -0,0 +1,19 @@
+package JOBSHEET5;
+import java.util.Scanner;
+
+public class genapGanjil27 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        int angka;
+        System.out.print("Masukkan angka : ");
+        angka = sc.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Angka genap");
+        } else {
+            System.out.println("Angka ganjil");
+        }
+        sc.close();
+    }
+}
