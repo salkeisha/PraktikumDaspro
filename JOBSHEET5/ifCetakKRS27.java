25ec43fbe0207b490bf22b511f3000dd13ba1a2a
salkeisha <keishasetiadi48@gmail.com>
2025-10-07 14:10:28 +0700

    Modifikasi Percobaan 1

--- JOBSHEET5/ifCetakKRS27.java
+++ JOBSHEET5/ifCetakKRS27.java
@@ -12,7 +12,8 @@ public class ifCetakKRS27 {
         if (uktLunas) {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-
+        } else {
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
         }
         sc.close();
     }
