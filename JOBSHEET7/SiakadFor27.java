4a3aef266b452b021ff8dd83bf75a615483ef7dc
salkeisha <keishasetiadi48@gmail.com>
2025-10-24 14:28:47 +0700

    update done

--- JOBSHEET7/SiakadFor27.java
+++ JOBSHEET7/SiakadFor27.java
@@ -6,6 +6,7 @@ public class SiakadFor27 {
         Scanner sc = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus=0, tidakLulus=0;
 
         for (int i=1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -16,9 +17,17 @@ public class SiakadFor27 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            //Modifikasi penambahan seleksi dalam perulangan
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
-        System.out.println("Nilai tertinggi: " + tertinggi);
-        System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Nilai tertinggi                            : " + tertinggi);
+        System.out.println("Nilai terendah                             : " + terendah);
+        System.out.println("Jumlah mahasiswa yang lulus adalah         : " + lulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus adalah   : " + tidakLulus);
         sc.close();
     }
     
