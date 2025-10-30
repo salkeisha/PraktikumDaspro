f2a868c2811a9e9051afd1062625fa44116944e7
salkeisha <keishasetiadi48@gmail.com>
2025-10-30 19:37:07 +0700

    update

--- JOBSHEET8/Tugas2.java
+++ JOBSHEET8/Tugas2.java
@@ -5,48 +5,40 @@ public class Tugas2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in) ;
 
-        System.out.print("Masukkan angka : ");
-        int n = sc.nextInt();
+        // Input angka harus lebih dari 3
+        int angka;
+        do {
+        System.out.print("Masukkan angka (lebih dari 3) : ");
+        angka = sc.nextInt();
+            if (angka < 3) {
+                System.out.println("Angka kurang dari 3. Coba lagi!");
+            }
+        } while (angka < 3);
 
-        // jika angka dibawah 10
-        if (n < 10) {
-        for (int i = 1; i <= n; i++) {
-            if (i==1||i==n) {
-                for (int j = 1; j <= n; j++) {
-                System.out.print(" " + n + " ");
+        //Jika angka kurang dari 10 maka spasi yg dibutuhkan hanya satu
+        if (angka < 10) {
+        for (int i = 1; i <= angka; i++) {
+            for (int j = 1; j <= angka; j++) {
+                if (i == 1 || j == 1 || i == angka || j == angka) {
+                    System.out.print(angka + " ");
+                } else {
+                    System.out.print("  ");
                 }
+            }
             System.out.println();
-            } else {
-                for (int j = 1; j <= n; j++) {
-                    if (j==1||j==n) {
-                        System.out.print(" " + n + " ");
-                    } else {
-                        System.out.print(" " + " " + " ");
-                    }
-                }
-                System.out.println();
-                }
-            }
-            
-        // jika angka diatas 10
+        }
+        //Jika angka lebih dari sama dengan 10 maka spasi yang dibutuhkan dua
         } else {
-            for (int i = 1; i <= n; i++) {
-                if (i==1||i==n) {
-                    for (int j = 1; j <= n; j++) {
-                        System.out.print(" " + n + " ");
-                }
-                        System.out.println();
-            } else {
-                for (int j = 1; j <= n; j++) {
-                    if (j==1||j==n) {
-                        System.out.print(" " + n + " ");
-                    } else {
-                        System.out.print(" " + "  " + " ");
-                    }
-                }
-                System.out.println();
+                    for (int i = 1; i <= angka; i++) {
+            for (int j = 1; j <= angka; j++) {
+                if (i == 1 || j == 1 || i == angka || j == angka) {
+                    System.out.print(angka + "  ");
+                } else {
+                    System.out.print("    ");
                 }
             }
+            System.out.println();
+        }
         }
         sc.close();
     }
