e2c1cefae67c592486d6d31e452cdd593277be79
salkeisha <keishasetiadi48@gmail.com>
2025-11-14 20:15:03 +0700

    update

--- JOBSHEET5/genapGanjil27.java
+++ JOBSHEET5/genapGanjil27.java
@@ -5,7 +5,7 @@ public class genapGanjil27 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
 
-        int angka, sisa;
+        int angka;
         System.out.print("Masukkan angka : ");
         angka = sc.nextInt();
 
@@ -14,13 +14,6 @@ public class genapGanjil27 {
         } else {
             System.out.println("Angka ganjil");
         }
-
-        // sisa = angka % 2;
-        // if (sisa == 1) {
-        //     System.out.println("Angka ganjil");
-        // } else {
-        //     System.out.println("Angka genap");
-        // }
 
         sc.close();
     }
